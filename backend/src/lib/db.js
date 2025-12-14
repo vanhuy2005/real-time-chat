42a4e55d6fbf602e5a5e8d10e2610a61fd768309
Văn Huy <nguyen.van.quang.huy.2105@gmail.com>
2025-12-14 18:18:33 +0700

    deploy version 1

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -6,5 +6,6 @@ export const connectDB = async () => {
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.log("MongoDB connection error:", error);
+    process.exit(1);
   }
 };
